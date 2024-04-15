8059216fe149ccbd84f54d09d13bf412277f95cb
Rando 87 <68273711+tharunyogenthra@users.noreply.github.com>
2024-04-15 19:34:11 +1000

    Added cache and token renewal system by saving refresh token and automatically refreshing that refresh token is the refresh_expiry_date is over. This feature is literally a life saver. Good job me (#5)
    Co-authored-by: Tharun Yogenthra <z5480907@ad.unsw.edu.au>

--- run.py
+++ run.py
@@ -5,9 +5,9 @@ REDIRECT_URL = "http://localhost:8888/callback"
 USER_ID = '31qxnttkgyllhr4jassvn3pdzj6q'
 
 sp = SpotifyAPIWrapper(client_id=CLIENT_ID, redirect_url=REDIRECT_URL)
-sp.authentication(scope=['playlist-read-private', 'playlist-read-collaborative']) # add cache
+sp.authentication(scope=['playlist-read-private', 'playlist-read-collaborative'])
 
-playlists = sp.get_user_playlists(user_id=USER_ID, limit=50, offset=3) # add user_id (limited use), limit, offset
+playlists = sp.get_user_playlists(user_id=USER_ID, limit=50, offset=3)
 
 for i in range(0, len(playlists)):
     print(playlists.items[i].name)
