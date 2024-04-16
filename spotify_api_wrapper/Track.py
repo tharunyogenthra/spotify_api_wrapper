4915851402f9d5f95e88787747f16c7d380f787e
Tharun Yogenthra <z5480907@ad.unsw.edu.au>
2024-04-16 10:30:06 +1000

    added functionality to print out tracks from spotify url

---
+++ spotify_api_wrapper/Track.py
@@ -0,0 +1,30 @@
+import requests
+from .SpotifyApiException import SpotifyApiException
+import json
+from .dataclasses.TrackDataClass import TrackDataClass
+
+class Track:
+    def __init__(self, auth_client):
+        self.auth_client = auth_client
+        
+    def fetch_web_api(self, endpoint, method='GET', body=None):
+        access_token = self.auth_client.access_token
+        headers = {
+            'Authorization': f'Bearer {access_token}'
+        }
+        response = requests.request(method, endpoint, headers=headers, json=body)
+        response_status_code = response.status_code        
+
+        if (response_status_code in [400, 401, 403, 404, 429]):
+            response_content = response.content
+            error_message = json.loads(response_content.decode('utf-8'))['error']['message']
+            raise SpotifyApiException(f'ERROR {response.status_code} {error_message}')
+        
+        return response.json()
+        
+    def get_user_tracks(self, url, limit, offset=0):
+        # 'https://api.spotify.com/v1/playlists/3cEYpjA9oz9GiPac4AsH4n/tracks?limit=10&offset=0'
+        endpoint = f'{url}?limit={limit}&offset={offset}'
+
+        return TrackDataClass(self.fetch_web_api(endpoint))
+        
